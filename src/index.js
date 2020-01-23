caf957002690e0e002d0270ba87fa0ca7f2328db
Jonty <jonty_wolf@hotmail.com>
2020-01-23 23:12:14 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.jsx";
+import "./styles/index.scss";
+
+ReactDOM.render(<App />, document.getElementById("root"));
