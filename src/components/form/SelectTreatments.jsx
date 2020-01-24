b19c5c119ed16281059b1489bcebdcc60b23e93e
Artwolf <jonty_wolf@hotmail.com>
2020-01-24 11:47:13 +0100

    staging

--- src/components/form/SelectTreatments.jsx
+++ src/components/form/SelectTreatments.jsx
@@ -6,38 +6,55 @@ class SelectTreatments extends React.Component {
         super(props);
 
         this.state = {
-            treatments: []
+            treatments: [],
+            selectedTreatments: []
         }
     }
 
     componentDidMount() {
+        this.getTreatmentList();
+    }
+
+    getTreatmentList() {
         fetch('http://reserved-backend.test/treatments')
             .then((response) => {
                 return response.json();
             })
             .then((data) => {
                 this.setState({treatments: data.data});
+                this.setState({selectedTreatments: [1]});
             })
             .catch((error) => {
                 console.error('Error:', error);
             });
-    }
+    };
 
     render() {
-        return(
+        return (
             <React.Fragment>
                 <Form>
-                <h1>Kies de behandelingen</h1>
-                {this.state.treatments.map((treatment, idx) => {
-                    return (
-                        <Form.Check
-                            key={idx}
-                            type={'checkbox'}
-                            id={`default-checkbox-${idx}`}
-                            label={`${treatment.name} (gem.tijdsduur: ${treatment.duration})`}
-                        />
-                    )
-                })}
+                    <h1>Kies de behandelingen</h1>
+                    <div className="selectedTreatments">
+                        {this.state.selectedTreatments.map((treatmentID, idx) => {
+                            let {name, duration} = this.state.treatments.find(treatment => treatment.id === treatmentID);
+
+                            return (
+                                <div className="selectedTreatment" key={idx}>
+                                    {`${name} (${duration})`}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    {this.state.treatments.map((treatment, idx) => {
+                        return (
+                            <Form.Check
+                                key={idx}
+                                type={'checkbox'}
+                                id={`default-checkbox-${idx}`}
+                                label={`${treatment.name} (gem.tijdsduur: ${treatment.duration})`}
+                            />
+                        )
+                    })}
                 </Form>
             </React.Fragment>
         );
